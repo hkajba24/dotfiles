e73bf259767cc325583c19a5471bf6b754359bcb
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-09-27 22:44:37 +0200

    Changed nvim setup

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,44 +1,19 @@
 return {
   {
-    "2nthony/vitesse.nvim",
+    "marko-cerovac/material.nvim",
     lazy = false,
     priority = 1000,
-    dependencies = {
-      "tjdevries/colorbuddy.nvim",
-    },
     config = function()
-      require("vitesse").setup({
-        comment_italics = true,
-        transparent_background = true,
+      require("material").setup({
+        disable = { background = true },
       })
     end,
   },
 
-  {
-    "dgox16/oldworld.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("oldworld").setup({
-        integrations = {
-          navic = true,
-          alpha = false,
-          rainbow_delimiters = false,
-        },
-        highlight_overrides = {
-          Normal = { bg = "NONE" },
-          NormalNC = { bg = "NONE" },
-          CursorLine = { bg = "#222128" },
-        },
-      })
-    end,
-  },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oldworld",
+      colorscheme = "material",
     },
   },
 }
