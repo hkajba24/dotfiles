b1e138cf1ab9efcae0a09dad6e716d53794a3d1a
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-12-01 00:03:05 +0100

    Updated neovim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,9 +11,44 @@ return {
   },
 
   {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanso").setup({
+        background = {
+          light = "pearl",
+          dark = "ink",
+        },
+      })
+    end,
+  },
+
+  {
+    "marko-cerovac/material.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("material").setup({
+        disable = {
+          background = true,
+        },
+        lualine_style = "stealth",
+      })
+      vim.g.material_style = "darker"
+    end,
+  },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    lazy = false,
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "material",
     },
   },
 }
